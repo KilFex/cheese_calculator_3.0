9c7ac01a73ea6bc5d95f9c9ef6ceaefdfc89feed
AnderH4ck <anderson2020tasilla@gmail.com>
2024-07-16 22:40:40 -0500

    El comienzo cap3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
 function App() {
-
   return (
-    <div>
-
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
